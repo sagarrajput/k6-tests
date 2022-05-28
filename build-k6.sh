bd246cc5a02cd181b9ba1b378c17da6fa5d2dc94
sagarrajput <sagar.rajput@medly.com>
2022-05-29 00:10:42 +0530

    added simple test

--- build-k6.sh
+++ build-k6.sh
@@ -5,5 +5,12 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 $HOME/.profile
 go version
+git clone https://github.com/grafana/xk6
 go install go.k6.io/xk6/cmd/xk6@latest
-xk6 build latest \ --with github.com/grafana/xk6-exec@latest \ --with github.com/avitalique/xk6-file@latest
+cd xk6; make
+./xk6 build latest \
+  --with github.com/grafana/xk6-exec@latest \
+  --with github.com/avitalique/xk6-file@latest
+cp k6 ../
+rm -rf ../xk6
+../k6 run ../tests/simple.js
---
+++ tests/simple.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+// A simple counter for http requests
+
+// you can specify stages of your test (ramp up/down patterns) through the options object
+// target is the number of VUs you are aiming for
+
+export const options = {
+  vus: 1,
+  duration: '2s',
+};
+
+export default function () {
+  // our HTTP request, note that we are saving the response to res, which can be accessed later
+
+  const res = http.get('http://test.k6.io');
+
+  sleep(1);
+
+  const checkRes = check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+}
