4eea5d18b9a2769eed58fabc0af3fa2d74ab4fdf
sagarrajput <sagar.rajput@medly.com>
2022-05-29 12:43:38 +0530

    Update build-k6.sh

--- build-k6.sh
+++ build-k6.sh
@@ -5,12 +5,13 @@ wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
 chmod 777 *.*
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
 export PATH=/usr/local/go/bin
+$HOME/.profile.
 go version
 go install go.k6.io/xk6/cmd/xk6@latest
 export GOPATH=/usr/local/go/bin
 export GOBIN=$GOPATH/bin
 export PATH=$GOPATH/bin:$PATH
-source ~/.bashrc
+$HOME/.profile.
 echo $GOPATH
 cd /usr/local/go/bin
 pwd
