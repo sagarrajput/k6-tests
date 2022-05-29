0cce2325d3b5873d57533f7810cbb1eeb5ea5592
sagarrajput <sagar.rajput@medly.com>
2022-05-29 12:48:08 +0530

    Update build-k6.sh

--- build-k6.sh
+++ build-k6.sh
@@ -3,17 +3,17 @@ apt-get install git
 eval echo ~$USER
 wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
 chmod 777 *.*
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export PATH=/usr/local/go/bin
+rm -rf /Users/root/go && tar -C /Users/root -xzf go1.18.2.linux-amd64.tar.gz
+export PATH=/Users/root/go/bin
 $HOME/.profile.
 go version
 go install go.k6.io/xk6/cmd/xk6@latest
-export GOPATH=/usr/local/go/bin
+export GOPATH=/Users/root/go/bin
 export GOBIN=$GOPATH/bin
 export PATH=$GOPATH/bin:$PATH
 $HOME/.profile.
 echo $GOPATH
-cd /usr/local/go/bin
+cd /Users/root/go/bin
 pwd
 xk6 build latest \
   --with github.com/grafana/xk6-exec@latest \
