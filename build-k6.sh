5497fe5a5c18e600521bfe163cfc219ac09958f1
sagarrajput <sagar.rajput@medly.com>
2022-05-29 16:33:30 +0530

    Update build-k6.sh

--- build-k6.sh
+++ build-k6.sh
@@ -3,5 +3,5 @@ apt-get install git
 eval echo ~$USER
 sudo apt-get install curl git mercurial make binutils bison gcc build-essential
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source /usr/local/.gvm/scripts/gvm
+source /root/.gvm/scripts/gvm
 gvm install go1.18.2
