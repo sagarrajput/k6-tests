64cc6bb128f19d83d74bc8e70f94610e206403d9
sagarrajput <sagar.rajput@medly.com>
2022-05-29 13:53:38 +0530

    move env logic to yml file

--- build-k6.sh
+++ build-k6.sh
@@ -4,12 +4,3 @@ eval echo ~$USER
 wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
 chmod 777 *.*
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export PATH=/usr/local/go/bin
-$HOME/.profile.
-go version
-go install go.k6.io/xk6/cmd/xk6@latest
-export GOPATH=/usr/local/go
-export GOBIN=$GOPATH/bin
-export PATH=$GOPATH/bin:$PATH
-$HOME/.profile.
-echo $GOPATH
