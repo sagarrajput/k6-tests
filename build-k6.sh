87cde16a8fc4198e684d379897ae27a40b0532c4
sagarrajput <sagar.rajput@medly.com>
2022-05-29 11:59:19 +0530

    Update build-k6.sh

--- build-k6.sh
+++ build-k6.sh
@@ -7,10 +7,10 @@ export PATH=/usr/local/go/bin
 source ~/.bash_profile
 go version
 go install go.k6.io/xk6/cmd/xk6@latest
-export GOPATH=/Users/username/go
+export GOPATH=/Users/root/go
 export PATH=$GOPATH/bin:$PATH
 source ~/.bash_profile
-cd /usr/local/go/bin
+cd /Users/root/go
 xk6 build latest \
   --with github.com/grafana/xk6-exec@latest \
   --with github.com/avitalique/xk6-file@latest
