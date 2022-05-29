5b9269810d6ed9813037227734befd1e18153b22
sagarrajput <sagar.rajput@medly.com>
2022-05-29 17:51:20 +0530

    Update build-k6.sh

--- build-k6.sh
+++ build-k6.sh
@@ -3,5 +3,5 @@ apt-get install git
 eval echo ~$USER
 wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
 chmod 777 *.*
-rm -rf /usr/root/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+rm -rf /usr/root/go && tar -C /usr/root -xzf go1.18.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/root/go/bin
