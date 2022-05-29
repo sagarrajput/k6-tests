2865377528ca9e3b382938041c6bd4b78f6bd755
sagarrajput <sagar.rajput@medly.com>
2022-05-29 16:22:23 +0530

    test

--- build-k6.sh
+++ build-k6.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt-get install git
 eval echo ~$USER
-wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
-chmod 777 *.*
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source /usr/local/.gvm/scripts/gvm
+gvm install go1.18.2
